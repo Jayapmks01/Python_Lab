86d4b19a78612c039a7b11e0f16726b0b8e7a723
Jaya Myzee <72689440+Jayapmks01@users.noreply.github.com>
2023-11-09 13:31:59 +0700

    Add files via upload

---
+++ Lab2.py
@@ -0,0 +1,49 @@
+print("")
+print("=====Biodata======")
+nama = 'asep'
+nim = '0097575678557'
+kelas = 'SI05'
+_0Alamat = """Kampung baru, kecamatan jagakarsa,
+nomer 10"""
+
+print(nama)
+print(nim)
+print(kelas)
+print(_0Alamat)
+
+
+print("")
+print("=====Badan Ideal======")
+tinggi = 1.6
+berat_badan = 60
+berat_ideal = berat_badan/(tinggi*tinggi)
+print("berat badan ideal kamu =", int(berat_ideal))
+
+print("")
+print("=====Suhu celcius ke Fahrenhet======")
+celcius = 37
+r_fahrenheit = (celcius*9/5)+32
+print(r_fahrenheit)
+
+print("")
+print("=====Luas Tabung======")
+pi = 3.14
+jari2 = 5
+tinggi = 10
+rumus =  pi*jari2*jari2*tinggi
+print(int(rumus))
+
+print("")
+print("=====Mencari Keliling Tabung======")
+pi = 3.14
+r = 5
+rumus = (2*pi*r)
+print(int(rumus))
+
+
+print("")
+print("=====Tutup Atas======")
+phi = 3.14
+jari2 = 20
+hitung = jari2 * jari2 * phi * 2
+print(int(hitung))
